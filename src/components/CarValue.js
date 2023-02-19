5b7d1b9487c81de580f7a24c3089aba006b6cc88
Koushik Dey <koushik.dey@live.com>
2023-02-19 18:37:29 +0200

    added value component

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,8 +1,20 @@
-
+import { useSelector } from 'react-redux';
 
 const CarValue = () => {
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) => {
+      return car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    });
+
+    return filteredCars.reduce((acc, car) => {
+      return acc + car.cost
+    }, 0);
+  });
+
   return (
-    <div>CarValue</div>
+    <div className="car-value">
+      Total Cost: ${totalCost}
+    </div>
   );
 }
 
