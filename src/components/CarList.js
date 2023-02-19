86c52bc7c6fe94371c0c6f8271b01e31bc23c251
Koushik Dey <koushik.dey@live.com>
2023-02-19 18:55:29 +0200

    added highlighting option of existing car

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,9 +3,15 @@ import { removeCar } from '../store';
 
 const CarList = () => {
   const dispatch = useDispatch();
-  const cars = useSelector(({cars: {data, searchTerm}}) => {
-    return data.filter((car) =>
-      car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  const { cars, name } = useSelector(({form, cars: {data, searchTerm}}) => {
+    const filteredCars = data.filter((car) =>
+      car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return {
+      cars: filteredCars,
+      name: form.name
+    };
   });
 
   const handleCarDelete = (car) => {
@@ -13,8 +19,11 @@ const CarList = () => {
   };
 
   const renderedCars = cars.map((car) => {
+    // Decide if the car should be bold
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
